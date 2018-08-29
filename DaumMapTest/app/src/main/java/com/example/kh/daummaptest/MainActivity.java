57a446ba601206532d78d5cb77bc5959e3c13b62
Niel89 <kth887@naver.com>
2018-08-29 16:47:34 +0900

    2018.08.29_태환
    현위치

--- DaumMapTest/app/src/main/java/com/example/kh/daummaptest/MainActivity.java
+++ DaumMapTest/app/src/main/java/com/example/kh/daummaptest/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.kh.daummaptest;
 
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +24,8 @@ import android.widget.Toast;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
+import java.util.List;
 
 public class MainActivity extends Activity implements MapView.MapViewEventListener {
 
@@ -28,30 +38,13 @@ public class MainActivity extends Activity implements MapView.MapViewEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 맵뷰
         mapView = (MapView)findViewById(R.id.daumMapView);
         mapView.setMapViewEventListener(this);
 
         mTapTextView = (TextView) findViewById(R.id.mTapTextView);
 
-/*        //지도 띄우기
-        MapView mapView = new MapView(this);
-        //mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.5396224, 129.31152759999998),true);
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading); // 현위치 표시
-        mapView.setShowCurrentLocationMarker(true);
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);*/
-
-/*        //마커
-        MapPOIItem marker = new MapPOIItem();
-        marker.setItemName("Default Marker!!!");
-        marker.setTag(0);
-        //marker.setMapPoint(MapPoint.mapPointWithGeoCoord(35.5396224, 129.31152759999998));
-        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양
-        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양
-
-        mapView.addPOIItem(marker);*/
     }
-
 
     @Override
     public void onMapViewInitialized(MapView mapView) {
@@ -91,6 +84,7 @@ public class MainActivity extends Activity implements MapView.MapViewEventListen
         MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();    // GPS로 표현
         MapPoint.PlainCoordinate mapPointScreenLocation = mapPoint.getMapPointScreenLocation(); // X, Y축으로 표현
 
+        mapView.removeAllPOIItems();
         MapPOIItem marker = new MapPOIItem();
         marker.setItemName("Default Marker!!!");
         marker.setTag(0);
@@ -140,4 +134,9 @@ public class MainActivity extends Activity implements MapView.MapViewEventListen
         return super.onOptionsItemSelected(item);
 
         }
+
+    // 현위치 클릭
+    public void btnMyLocationOnClick(View view) {
+        mapView.setShowCurrentLocationMarker(true);
+    }
 }
